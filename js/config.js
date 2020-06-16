989f1a53d1df7ff28d68df2150214870e2d45293
iranakam <zcqozolput47djofoknd@gmail.com>
2020-06-17 01:22:28 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_bJkQQT9Fi', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2pofshb9r32r40pjbfbq7mbuhr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
